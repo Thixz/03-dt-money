fd577b6f64a44537e00cffe3b78bc1d92a856a19
Thiago Albuquerque <thiago.albuquerque@iterative.com.br>
2025-09-19 13:19:26 -0300

    [update] Adicionado headers, summary e transaction. Projeto configurado.

---
+++ src/pages/Transactions/index.tsx
@@ -0,0 +1,31 @@
+import { Header } from "../../components/Header";
+import { Summary } from "../Summary";
+import { PriceHighlight, TransactionsContainer, TransactionsTable } from "./styles";
+
+export function Transactions() {
+  return (
+    <div>
+      <Header />
+      <Summary />
+
+      <TransactionsContainer>
+        <TransactionsTable>
+          <tbody>
+            <tr>
+              <td width="45%">Desenvolvimento de site</td>
+              <PriceHighlight variant="income">R$ 12.000,00</PriceHighlight >
+              <td>Venda</td>
+              <td>13/04/2022</td>
+            </tr>
+            <tr>
+              <td width="45%">Hamburguer</td>
+              <PriceHighlight variant="outcome">- R$ 59,00</PriceHighlight >
+              <td>Alimentação</td>
+              <td>10/04/2022</td>
+            </tr>
+          </tbody>
+        </TransactionsTable>
+      </TransactionsContainer>
+    </div>
+  );
+}
